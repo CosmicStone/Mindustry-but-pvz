6facae3085eea0e64f66f778938975374ec22587
cosmic_stone <178169421@qq.com>
2026-03-02 23:26:50 +0800

    second commit

--- core/src/main/java/com/pvz/entity/turret/TowerSystems.java
+++ core/src/main/java/com/pvz/entity/turret/TowerSystems.java
@@ -6,7 +6,6 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.pvz.entity.Components;
 import com.pvz.entity.Components.AttackComponent;
 import com.pvz.entity.Components.PositionComponent;
